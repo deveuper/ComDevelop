aa6aa7dcf377c4467d9b658014f7208875d3f980
deveuper <249068538@qq.com>
2018-05-07 14:11:37 +0800

    新增相机跟随物体脚本 CameraFollow

---
+++ ComDevelop/Assets/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ComDevelop.ComTool
+{
+    //摄像机跟随物体移动
+    public class CameraFollow : MonoBehaviour
+    {
+        //跟随的摄像机
+        public Transform cameraFollow;
+        //跟随的物体
+        public Transform followTarget;
+        //相机跟随的缓动差值
+        public float LerpValue = 10;
+        //摄像机与物体的间距向量
+        private Vector3 offset;
+        public bool lookRotate = false;
+
+        // Use this for initialization
+        void Start()
+        {
+            offset = cameraFollow.position - followTarget.position;
+        }
+
+        // Update is called once per frame
+        private void LateUpdate()
+        {
+            cameraFollow.position = Vector3.Lerp(cameraFollow.position, (offset + followTarget.position), LerpValue * Time.deltaTime);
+            if (lookRotate) cameraFollow.LookAt(followTarget);
+        }
+    }
+}
---
+++ ComDevelop/Assets/ComDevelop/ComTool/Camera/Camera3rd/ThirdPlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        InputControl();
+    }
+    private void InputControl()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+       
+    }
+}
